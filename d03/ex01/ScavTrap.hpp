cd0abd093d956e7a401c863e47071ff72b273ff2
gbouwen <gijsbouwen@gmail.com>
2020-08-25 16:25:40 +0200

    finished ex01

---
+++ d03/ex01/ScavTrap.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ScavTrap.hpp                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/08/25 12:25:38 by gbouwen       #+#    #+#                 */
+/*   Updated: 2020/08/25 16:12:30 by gbouwen       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include <iostream>
+
+class ScavTrap
+{
+
+public:
+
+	ScavTrap(void);
+	ScavTrap(std::string name);
+	ScavTrap(ScavTrap const &src);
+	~ScavTrap(void);
+
+	ScavTrap	&operator=(ScavTrap const &rhs);
+
+	void	rangedAttack(std::string const &target);
+	void	meleeAttack(std::string const &target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+	void	challengeNewcomer(std::string const &newcomer);
+
+private:
+
+	unsigned int	_hitPoints;
+	unsigned int	_maxHitPoints;
+	unsigned int	_energyPoints;
+	unsigned int	_maxEnergyPoints;
+	unsigned int	_level;
+	std::string		_name;
+	unsigned int	_meleeAttackDamage;
+	unsigned int	_rangedAttackDamage;
+	unsigned int	_armorDamageReduction;
+};
+
+#endif
