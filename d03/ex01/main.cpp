52ebb32f0157f9bbd9391f97e63134ef5fda9e45
gbouwen <gijsbouwen@gmail.com>
2020-08-26 16:08:54 +0200

    made main more clear

--- d03/ex01/main.cpp
+++ d03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/25 15:09:37 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/08/26 12:08:13 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/08/26 16:08:29 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,19 @@ int	main(void)
 		FragTrap	fragThree("FRAGTRAP");
 
 		fragOne = FragTrap("name");
+		std::cout << "-----------" << std::endl;
 		fragOne.rangedAttack("Dog");
 		fragTwo.meleeAttack("Bird");
+		std::cout << "-----------" << std::endl;
 		fragThree.vaultHunter_dot_exe("Robot");
+		std::cout << "-----------" << std::endl;
 		fragOne.takeDamage(25);
 		fragTwo.takeDamage(3);
+		std::cout << "-----------" << std::endl;
 		fragThree.takeDamage(500);
 		fragThree.beRepaired(50);
 		fragOne.beRepaired(5000);
+		std::cout << "-----------" << std::endl;
 		fragThree.vaultHunter_dot_exe("Robot");
 		fragThree.vaultHunter_dot_exe("Robot");
 		fragThree.vaultHunter_dot_exe("Robot");
@@ -41,14 +46,18 @@ int	main(void)
 		ScavTrap	scavThree("SCAVTRAP");
 
 		scavOne = ScavTrap("scavvie");
+		std::cout << "-----------" << std::endl;
 		scavOne.rangedAttack("Lion");
 		scavTwo.meleeAttack("Bear");
+		std::cout << "-----------" << std::endl;
 		scavThree.challengeNewcomer("A");
 		scavThree.challengeNewcomer("B");
 		scavThree.challengeNewcomer("C");
+		std::cout << "-----------" << std::endl;
 		scavOne.takeDamage(20);
 		scavTwo.takeDamage(2);
 		scavThree.takeDamage(200);
+		std::cout << "-----------" << std::endl;
 		scavThree.beRepaired(50);
 		scavOne.beRepaired(5000);
 	}
