737cfd996250475cf5d78b2f3f7b62136ff992f4
gbouwen <gijsbouwen@gmail.com>
2020-08-26 16:07:36 +0200

    made main more clear

--- d03/ex00/main.cpp
+++ d03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/25 15:09:37 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/08/25 15:35:20 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/08/26 16:06:57 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,19 @@ int	main(void)
 	FragTrap	three("CLAPTRAP");
 
 	one = FragTrap("name");
+	std::cout << "----------------" << std::endl;
 	one.rangedAttack("Dog");
 	two.meleeAttack("Bird");
+	std::cout << "----------------" << std::endl;
 	three.vaultHunter_dot_exe("Robot");
+	std::cout << "----------------" << std::endl;
 	one.takeDamage(25);
 	two.takeDamage(3);
+	std::cout << "----------------" << std::endl;
 	three.takeDamage(500);
 	three.beRepaired(50);
 	one.beRepaired(5000);
+	std::cout << "----------------" << std::endl;
 	three.vaultHunter_dot_exe("Robot");
 	three.vaultHunter_dot_exe("Robot");
 	three.vaultHunter_dot_exe("Robot");
