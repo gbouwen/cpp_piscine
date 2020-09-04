c29507439d9b07c18dbe5bb654dc86570ddc27ab
gbouwen <gijsbouwen@gmail.com>
2020-09-04 14:06:06 +0200

    improved main + two functions

--- d04/ex02/main.cpp
+++ d04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/03 12:05:39 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/09/04 11:11:31 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/09/04 14:05:38 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,28 @@ int	main(void)
 {
 	ISpaceMarine	*bob = new TacticalMarine;
 	ISpaceMarine	*jim = new AssaultTerminator;
+	ISpaceMarine	*boba = new TacticalMarine;
+	ISpaceMarine	*bobb = new TacticalMarine;
+	ISpaceMarine	*bobc = new TacticalMarine;
+	ISpaceMarine	*bobd = new TacticalMarine;
+	ISpaceMarine	*bobe = new TacticalMarine;
+	ISpaceMarine	*bobg = new TacticalMarine;
 
 	ISquad *vlc = new Squad;
 	vlc->push(bob);
+	vlc->push(bob);
+	vlc->push(NULL);
 	vlc->push(jim);
+	vlc->push(boba);
+	vlc->push(bobb);
+	vlc->push(bobc);
+	vlc->push(bobd);
+	vlc->push(bobe);
+	vlc->push(bobg);
 
+	ISpaceMarine	*no = vlc->getUnit(12);
+	if (no == NULL)
+		std::cout << "ERROR: Couldn't get unit" << std::endl;
 	for (int i = 0; i < vlc->getCount(); i++)
 	{
 		ISpaceMarine	*cur = vlc->getUnit(i);
