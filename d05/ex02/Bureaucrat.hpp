21bc725695692e7495624027c7d4c4a2279c380e
gbouwen <gijsbouwen@gmail.com>
2020-09-14 14:45:46 +0200

    ready to start ex02

---
+++ d05/ex02/Bureaucrat.hpp
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Bureaucrat.hpp                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/09/14 10:19:19 by gbouwen       #+#    #+#                 */
+/*   Updated: 2020/09/14 14:30:34 by gbouwen       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <exception>
+# include "Form.hpp"
+
+class Form;
+
+class Bureaucrat
+{
+
+public:
+
+	Bureaucrat(std::string name, unsigned int grade);
+	Bureaucrat(Bureaucrat const &src);
+	~Bureaucrat(void);
+
+	Bureaucrat	&operator=(Bureaucrat const &rhs);
+
+	std::string		getName(void) const;
+	unsigned int	getGrade(void) const;
+
+	void			incrementGrade(void);
+	void			decrementGrade(void);
+	void			signForm(Form &form) const;
+
+	class GradeTooHighException : public std::exception
+	{
+
+	public:
+
+		GradeTooHighException(std::string error);
+
+		const char	*what(void) const noexcept;
+
+	private:
+
+		std::string	_errorMessage;
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+
+	public:
+
+		GradeTooLowException(std::string error);
+
+		const char	*what(void) const noexcept;
+
+	private:
+
+		std::string	_errorMessage;
+	};
+
+private:
+
+	Bureaucrat(void);
+
+	std::string		_name;
+	unsigned int	_grade;
+
+};
+
+std::ostream	&operator<<(std::ostream &o, Bureaucrat const &src);
+
+#endif
---
+++ d05/ex02/Form.cpp
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Form.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/09/14 13:36:47 by gbouwen       #+#    #+#                 */
+/*   Updated: 2020/09/14 14:29:32 by gbouwen       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+Form::Form(void)
+{
+	return ;
+}
+
+Form::Form(std::string name, unsigned int gradeToSign, unsigned int gradeToExecute) : _signed(false)
+{
+	this->_name = name;
+	if (gradeToSign < 1 || gradeToExecute < 1)
+		throw (Form::GradeTooHighException("Grade too high"));
+	if (gradeToSign > 150 || gradeToExecute > 150)
+		throw (Form::GradeTooLowException("Grade too low"));
+	this->_gradeToSign = gradeToSign;
+	this->_gradeToExecute = gradeToExecute;
+}
+
+Form::Form(Form const &src)
+{
+	*this = src;
+	return ;
+}
+
+Form::~Form(void)
+{
+	return ;
+}
+
+Form	&Form::operator=(Form const &rhs)
+{
+	if (this != &rhs)
+	{
+		this->_name = rhs._name;
+		this->_signed = rhs._signed;
+		this->_gradeToSign = rhs._gradeToSign;
+		this->_gradeToExecute = rhs._gradeToExecute;
+	}
+	return (*this);
+}
+
+std::string		Form::getName(void) const
+{
+	return (this->_name);
+}
+
+bool			Form::getSigned(void) const
+{
+	return	(this->_signed);
+}
+
+unsigned int	Form::getGradeToSign(void) const
+{
+	return (this->_gradeToSign);
+}
+
+unsigned int	Form::getGradeToExecute(void) const
+{
+	return (this->_gradeToExecute);
+}
+
+void			Form::beSigned(Bureaucrat const &src)
+{
+	if (src.getGrade() > this->_gradeToSign)
+		throw (Form::GradeTooLowException("Grade too low"));
+	this->_signed = true;
+	return ;
+}
+
+Form::GradeTooHighException::GradeTooHighException(std::string error)
+{
+	this->_errorMessage = error;
+	return ;
+}
+
+const char	*Form::GradeTooHighException::what(void) const noexcept
+{
+	return (this->_errorMessage.c_str());
+}
+
+Form::GradeTooLowException::GradeTooLowException(std::string error)
+{
+	this->_errorMessage = error;
+	return ;
+}
+
+const char	*Form::GradeTooLowException::what(void) const noexcept
+{
+	return (this->_errorMessage.c_str());
+}
+
+std::ostream	&operator<<(std::ostream &o, Form const &src)
+{
+	std::cout << src.getName();
+	if (src.getSigned())
+		std::cout << " is signed - ";
+	else
+		std::cout << " is not signed - ";
+	std::cout << "grade required to sign: " << src.getGradeToSign() << " - ";
+	std::cout << "grade required to execute: " << src.getGradeToExecute() << std::endl;
+	return (o);
+}
