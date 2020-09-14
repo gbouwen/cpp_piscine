b2d22564c3f65672cca2220a66a940dd68046f26
gbouwen <gijsbouwen@gmail.com>
2020-09-14 12:37:23 +0200

    finished ex00

---
+++ d05/ex00/Bureaucrat.hpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Bureaucrat.hpp                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/09/14 10:19:19 by gbouwen       #+#    #+#                 */
+/*   Updated: 2020/09/14 12:27:41 by gbouwen       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <exception>
+
+class Bureaucrat
+{
+
+public:
+
+	Bureaucrat(std::string name, unsigned int grade);
+	Bureaucrat(Bureaucrat const &src);
+	~Bureaucrat(void);
+
+	Bureaucrat	&operator=(Bureaucrat const &rhs);
+
+	std::string		getName(void) const;
+	unsigned int	getGrade(void) const;
+
+	void			incrementGrade(void);
+	void			decrementGrade(void);
+
+	class GradeTooHighException : public std::exception
+	{
+
+	public:
+
+		GradeTooHighException(std::string error);
+		const char	*what(void) const noexcept;
+
+	private:
+
+		std::string	_errorMessage;
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+
+	public:
+
+		GradeTooLowException(std::string error);
+		const char	*what(void) const noexcept;
+
+	private:
+
+		std::string	_errorMessage;
+	};
+
+private:
+
+	Bureaucrat(void);
+	std::string		_name;
+	unsigned int	_grade;
+
+};
+
+std::ostream	&operator<<(std::ostream &o, Bureaucrat const &src);
+
+#endif
---
+++ d05/ex00/main.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/09/14 12:21:16 by gbouwen       #+#    #+#                 */
+/*   Updated: 2020/09/14 12:37:04 by gbouwen       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int	main(void)
+{
+	try
+	{
+		Bureaucrat	bob("bob", 5);
+
+		std::cout << bob;
+		bob.incrementGrade();
+		std::cout << bob;
+		bob.incrementGrade();
+		std::cout << bob;
+		bob.incrementGrade();
+		std::cout << bob;
+		bob.incrementGrade();
+		std::cout << bob;
+		bob.incrementGrade();
+		std::cout << bob;
+	}
+	catch (std::exception &exception)
+	{
+		std::cout << "Error: " << exception.what() << std::endl;
+	}
+
+	try
+	{
+		Bureaucrat	bill("bill", 500);
+	}
+	catch (std::exception &exception)
+	{
+		std::cout << "Error: " << exception.what() << std::endl;
+	}
+	return (0);
+}
