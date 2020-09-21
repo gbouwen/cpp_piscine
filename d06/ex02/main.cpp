cf112206bf88f5fb36d1f77a745c176380ab77d2
gbouwen <gijsbouwen@gmail.com>
2020-09-21 17:32:54 +0200

    ready to start ex02

---
+++ d06/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/09/21 17:30:16 by gbouwen       #+#    #+#                 */
+/*   Updated: 2020/09/21 17:32:28 by gbouwen       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+//Base	*generate(void)
+//{
+
+//}
+
+//void	identify_from_pointer(Base *p)
+//{
+
+//}
+
+void	identify_from_reference(Base &p)
+
+int	main(void)
+{
+	return (0);
+}
