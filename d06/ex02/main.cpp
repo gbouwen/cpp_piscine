b2e456a877dc7a3d8a3c4ff58facb21de131777e
gbouwen <gijsbouwen@gmail.com>
2020-09-22 13:00:25 +0200

    ex02 done

--- d06/ex02/main.cpp
+++ d06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/21 17:30:16 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/09/21 17:51:55 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/09/22 12:58:24 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "B.hpp"
 #include "C.hpp"
 #include <cstdlib>
+#include <iostream>
 
 Base	*generate(void)
 {
@@ -26,23 +27,59 @@ Base	*generate(void)
 	return (new C);
 }
 
-// use dynamic cast to check the type
-// catch the exceptions in main
+void	identify_from_pointer(Base *p)
+{
+	A	*a = dynamic_cast<A *>(p);
+	B	*b = dynamic_cast<B *>(p);
+	C	*c = dynamic_cast<C *>(p);
 
-//void	identify_from_pointer(Base *p)
-//{
+	if (a)
+		std::cout << "A" << std::endl;
+	if (b)
+		std::cout << "B" << std::endl;
+	if (c)
+		std::cout << "C" << std::endl;
+}
 
-//}
+void	identify_from_reference(Base &p)
+{
+	try
+	{
+		A	a = dynamic_cast<A &>(p);
+		std::cout << "A" << std::endl;
+	}
+	catch (std::exception badCast)
+	{
 
-//void	identify_from_reference(Base &p)
-//{
+	}
+	try
+	{
+		B	b = dynamic_cast<B &>(p);
+		std::cout << "B" << std::endl;
+	}
+	catch (std::exception badCast)
+	{
 
-//}
+	}
+	try
+	{
+		C	c = dynamic_cast<C &>(p);
+		std::cout << "C" << std::endl;
+	}
+	catch (std::exception badCast)
+	{
+
+	}
+
+}
 
 int	main(void)
 {
-	Base *base = new Base;
+	Base *base = new Base();
 
+	srand(time(NULL));
 	base = generate();
+	identify_from_pointer(base);
+	identify_from_reference(*base);
 	return (0);
 }
