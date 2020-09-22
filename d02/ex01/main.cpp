a689a7105a4170c748993e7ee39b9bf956db2a27
gbouwen <gijsbouwen@gmail.com>
2020-09-22 10:48:14 +0200

    added Makefile

---
+++ d02/ex01/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/09/22 10:43:39 by gbouwen       #+#    #+#                 */
+/*   Updated: 2020/09/22 10:46:57 by gbouwen       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	Fixed		a;
+	Fixed const	b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
+
+	a = Fixed(1234.4321f);
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	return (0);
+}
