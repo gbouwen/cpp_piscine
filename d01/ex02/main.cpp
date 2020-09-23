ce4ce001fc62c38a3893ed83bbbe065fcaaa114b
gbouwen <gijsbouwen@gmail.com>
2020-09-23 10:49:34 +0200

    improved main

--- d01/ex02/main.cpp
+++ d01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gbouwen <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/03 13:36:05 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/08/18 15:14:45 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/09/23 10:49:08 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,35 @@
 
 int	main(void)
 {
-	ZombieEvent	event;
+	{
+		std::cout << "--- Start zombie on stack ---" << std::endl;
 
-	event.setZombieType("pirate");
-	Zombie	*zombieA = event.newZombie("haha");
-	zombieA->announce();
-	delete zombieA;
+		Zombie	bob = Zombie("Water", "Bob");
+		bob.announce();
+	}
+	std::cout << "--- End zombie on stack ---" << std::endl << std::endl;
 
-	Zombie	zombieB = Zombie("TYPE", "NAME");
-	zombieB.announce();
+	{
+		std::cout << "--- Start zombies on heap ---" << std::endl;
+		ZombieEvent	event;
+		event.setZombieType("Fire");
+		Zombie	*joe = event.newZombie("Joe");
+		joe->announce();
+		delete (joe);
 
-	Zombie	*zombieC = event.randomChump();
-	zombieC->announce();
-	delete zombieC;
-	Zombie	*zombieD = event.randomChump();
-	zombieD->announce();
-	delete zombieD;
-	Zombie	*zombieE = event.randomChump();
-	zombieE->announce();
-	delete zombieE;
-	Zombie	*zombieF = event.randomChump();
-	zombieF->announce();
-	delete zombieF;
+		Zombie	*randomFirst = event.randomChump();
+		randomFirst->announce();
+		delete (randomFirst);
 
+		Zombie	*randomSecond = event.randomChump();
+		randomSecond->announce();
+		delete (randomSecond);
+
+		Zombie	*randomThird = event.randomChump();
+		randomThird->announce();
+		delete (randomThird);
+
+	}
+	std::cout << "--- End zombies on heap ---" << std::endl;
 	return (0);
 }
