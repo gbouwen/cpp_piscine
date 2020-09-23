7c229593d5c11e3f97b3c5c6cdbf90cdd06298bd
gbouwen <gijsbouwen@gmail.com>
2020-09-23 11:25:10 +0200

    added const

--- d01/ex05/Human.cpp
+++ d01/ex05/Human.cpp
@@ -6,7 +6,7 @@
 /*   By: gbouwen <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/03 17:51:00 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/08/24 11:52:20 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/09/23 11:24:44 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ Human::~Human(void)
 	return ;
 }
 
-Brain const		&Human::getBrain(void)
+Brain const		&Human::getBrain(void) const
 {
 	return (this->_brain);
 }
--- d01/ex05/Human.hpp
+++ d01/ex05/Human.hpp
@@ -6,7 +6,7 @@
 /*   By: gbouwen <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/03 17:51:04 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/08/24 11:52:44 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/09/23 11:24:48 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ public:
 	Human(void);
 	~Human(void);
 
-	Brain const &getBrain(void);
+	Brain const &getBrain(void) const;
 	Brain const *identify(void) const;
 
 private:
