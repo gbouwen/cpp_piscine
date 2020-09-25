a388a6ade3a4a81d27794b5984521f6652eba238
gbouwen <gijsbouwen@gmail.com>
2020-09-25 11:51:19 +0200

    commented the awesome class

--- module07/ex00/whatever.cpp
+++ module07/ex00/whatever.cpp
@@ -6,32 +6,32 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/23 13:36:58 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/09/25 11:34:17 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/09/25 11:51:02 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-class
-Awesome
-{
+//class
+//Awesome
+//{
 
-public:
+//public:
 
-	Awesome( int n ) : _n( n ) { return ; }
-	int getN() const { return (this->_n); }
+	//Awesome( int n ) : _n( n ) { return ; }
+	//int getN() const { return (this->_n); }
 
-	bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
-	bool operator!=( Awesome const & rhs ) const { return (this->_n != rhs._n); }
-	bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
-	bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
-	bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
-	bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+	//bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+	//bool operator!=( Awesome const & rhs ) const { return (this->_n != rhs._n); }
+	//bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+	//bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+	//bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+	//bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
 
-private:
+//private:
 
-	int _n;
-};
+	//int _n;
+//};
 
 
 template <typename T>
@@ -76,23 +76,23 @@ int	main(void)
 	std::cout << "max of " << c << " and " << d << " is: " << max(c, d) << std::endl;
 	std::cout << "---------------------------------" << std::endl;
 
-	Awesome e(7);
-	Awesome f(11);
+	//Awesome e(7);
+	//Awesome f(11);
 
-	std::cout << "e before swap: " << e.getN() << std::endl;
-	std::cout << "f before swap: " << f.getN() << std::endl;
-	swap(e, f);
-	std::cout << "Swapping..." << std::endl;
-	std::cout << "e after swap: " << e.getN() << std::endl;
-	std::cout << "f after swap: " << f.getN() << std::endl;
-	std::cout << "---------------------------------" << std::endl;
+	//std::cout << "e before swap: " << e.getN() << std::endl;
+	//std::cout << "f before swap: " << f.getN() << std::endl;
+	//swap(e, f);
+	//std::cout << "Swapping..." << std::endl;
+	//std::cout << "e after swap: " << e.getN() << std::endl;
+	//std::cout << "f after swap: " << f.getN() << std::endl;
+	//std::cout << "---------------------------------" << std::endl;
 
-	std::cout << "min of " << e.getN() << " and " << f.getN() << " is: ";
-	Awesome result = min(e, f);
-	std::cout << result.getN() << std::endl;
-	std::cout << "---------------------------------" << std::endl;
-	std::cout << "max of " << e.getN() << " and " << f.getN() << " is: ";
-	result = max(e, f);
-	std::cout << result.getN() << std::endl;
+	//std::cout << "min of " << e.getN() << " and " << f.getN() << " is: ";
+	//Awesome result = min(e, f);
+	//std::cout << result.getN() << std::endl;
+	//std::cout << "---------------------------------" << std::endl;
+	//std::cout << "max of " << e.getN() << " and " << f.getN() << " is: ";
+	//result = max(e, f);
+	//std::cout << result.getN() << std::endl;
 	return (0);
 }
