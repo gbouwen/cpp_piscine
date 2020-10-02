a645e5b0205467fc1fabf0ecf6a8124606e9fe2e
Gijs Bouwen <gijsbouwen@gmail.com>
2020-10-02 11:30:56 +0200

    think this is it

--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -6,49 +6,117 @@
 /*   By: gbouwen <gbouwen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/29 16:43:44 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/10/02 10:36:28 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/10/02 11:30:22 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <deque>
 #include <list>
 
 void	subjectMain(void)
 {
-	MutantStack<int> mstack;
+	MutantStack<int> mStack;
 
-	mstack.push(5);
-	mstack.push(17);
+	mStack.push(17);
+	std::cout << "pushed 17 to stack" << std::endl;
 
-	std::cout << mstack.top() << std::endl;
+	std::cout << "mStack.top(): " << mStack.top() << std::endl;
+	std::cout << "size: " << mStack.size() << std::endl;
 
-	mstack.pop();
+	mStack.pop();
+	std::cout << "pop" << std::endl;
+	std::cout << "size: " << mStack.size() << std::endl;
 
-	std::cout << mstack.size() << std::endl;
+	mStack.push(3);
+	std::cout << "pushed 3 on stack" << std::endl;
+	mStack.push(5);
+	std::cout << "pushed 5 on stack" << std::endl;
+	mStack.push(737);
+	std::cout << "pushed 737 on stack" << std::endl;
+	mStack.push(0);
+	std::cout << "pushed 0 on stack" << std::endl;
+	std::cout << "size: " << mStack.size() << std::endl;
 
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	mstack.push(0);
-
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
+	MutantStack<int>::iterator it = mStack.begin();
+	MutantStack<int>::iterator ite = mStack.end();
 
 	++it;
 	--it;
+	std::cout << "printing with iterator: " << std::endl;
 	while (it != ite)
 	{
 		std::cout << *it << std::endl;
 		++it;
 	}
-	std::stack<int> s(mstack);
-	s.push(100);
-	std::cout << s.top() << std::endl;
+	std::stack<int> s(mStack);
+	return ;
+}
 
-	std::stack<int> s2 = mstack;
-	s2.push(100);
-	std::cout << s2.top() << std::endl;
+void	extraMain(void)
+{
+	MutantStack<std::string>	mStack;
 
+	mStack.push("world");
+	std::cout << "pushed 'world' on stack" << std::endl;
+	mStack.push("hello");
+	std::cout << "pushed 'hello' on stack" << std::endl;
+
+	MutantStack<std::string>::const_reverse_iterator	crit = mStack.rbegin();
+	MutantStack<std::string>::const_reverse_iterator	crite = mStack.rend();
+
+	std::cout << "printing with const_reverse_iterator:" << std::endl;
+	while (crit != crite)
+	{
+		std::cout << *crit << std::endl;
+		crit++;
+	}
+	std::cout << "---" << std::endl;
+
+	MutantStack<std::string>  mStack2 = mStack;
+	std::cout << "used overloaded assignation operator" << std::endl;
+
+	MutantStack<std::string>::reverse_iterator	rit = mStack2.rbegin();
+	MutantStack<std::string>::reverse_iterator	rite = mStack2.rend();
+	std::cout << "printing with reverse_iterator" << std::endl;
+	while (rit != rite)
+	{
+		std::cout << *rit << std::endl;
+		rit++;
+	}
+
+	return ;
+}
+
+void	extraMainTwo(void)
+{
+	MutantStack<int>	mStack;
+
+	mStack.push(5);
+	std::cout << "pushed 5 on stack" << std::endl;
+	mStack.push(10);
+	std::cout << "pushed 10 on stack" << std::endl;
+
+	MutantStack<int>		mStack2(mStack);
+	std::cout << "used copy constructor" << std::endl;
+
+	MutantStack<int>::iterator	it = mStack.begin();
+	MutantStack<int>::iterator	ite = mStack.end();
+	std::cout << "printing with iterator: " << std::endl;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		it++;
+	}
+	std::cout << "---" << std::endl;
+	MutantStack<int>::const_iterator	cit = mStack.begin();
+	MutantStack<int>::const_iterator cite = mStack.end();
+	std::cout << "printing with const_iterator: " << std::endl;
+	while (cit != cite)
+	{
+		std::cout << *cit << std::endl;
+		cit++;
+	}
 	return ;
 }
 
@@ -57,14 +125,12 @@ int main()
 	std::cout << "--- Testing subject main ---" << std::endl;
 	subjectMain();
 	std::cout << "----------------------------" << std::endl;
+	std::cout << "--- Testing extra main ---" << std::endl;
+	extraMain();
+	std::cout << "--------------------------" << std::endl;
+	std::cout << "--- Testing extra main 2---" << std::endl;
+	extraMainTwo();
+	std::cout << "--------------------------" << std::endl;
 
-	MutantStack<int>	s;
-
-	s.push(5);
-	std::cout << s.top() << std::endl;
-
-	std::stack< int, std::list<int> > a;
-	a.push(5);
-	std::cout << a.top() << std::endl;
 	return (0);
 }
